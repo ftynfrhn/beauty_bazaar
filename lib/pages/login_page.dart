c92e09b604d94149bf9a30d2d2fd2b8b315ae1e6
ftynfrhn <elfarhana900@gmail.com>
2024-06-24 23:23:59 +0800

    resolved overflow error

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,9 +4,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:beauty_bazaar/helper/helper_functions.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget { 
+class LoginPage extends StatefulWidget {
   final void Function()? onTap;
-  
+
   const LoginPage({super.key, required this.onTap});
 
   @override
@@ -16,16 +16,15 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   // text controllers
   final TextEditingController emailController = TextEditingController();
-
-  final TextEditingController passwordController = TextEditingController();  
+  final TextEditingController passwordController = TextEditingController();
 
   // login method
   void login() async {
-    //show loading circle
+    // show loading circle
     showDialog(
-      context: context, 
+      context: context,
       builder: (context) => const Center(
-      child: CircularProgressIndicator(),
+        child: CircularProgressIndicator(),
       ),
     );
 
@@ -38,11 +37,8 @@ class _LoginPageState extends State<LoginPage> {
       );
 
       // pop loading circle
-      if(context.mounted) Navigator.pop(context);
-    } 
-
-    // display any errors
-    on FirebaseAuthException catch (e) {
+      if (context.mounted) Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
       // pop loading circle
       Navigator.pop(context);
 
@@ -56,59 +52,64 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.only(
+            left: 25.0,
+            right: 25.0,
+            top: 25.0,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 25.0,
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
               // logo
               Icon(
                 Icons.person,
                 size: 80,
                 color: Theme.of(context).colorScheme.inversePrimary,
               ),
-          
+
               const SizedBox(height: 25),
-          
+
               // app name
               const Text(
                 "Beauty Bazaar",
                 style: TextStyle(fontSize: 24),
               ),
-          
+
               const SizedBox(height: 50),
-          
+
               // email textfield
               MyTextField(
-                hintText: "Email", 
-                obscureText: false, 
+                hintText: "Email",
+                obscureText: false,
                 controller: emailController,
               ),
-                        
+
               const SizedBox(height: 10),
 
               // password textfield
               MyTextField(
-                hintText: "Password", 
-                obscureText: true, 
+                hintText: "Password",
+                obscureText: true,
                 controller: passwordController,
               ),
-          
+
               const SizedBox(height: 10),
 
               // forgot password
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                    Text(
-                      "Forgot password?",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary)
+                  Text(
+                    "Forgot password?",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary,
                     ),
+                  ),
                 ],
               ),
-          
+
               const SizedBox(height: 25),
 
               // sign in button
@@ -116,17 +117,19 @@ class _LoginPageState extends State<LoginPage> {
                 text: "Login",
                 onTap: login,
               ),
-          
+
               const SizedBox(height: 25),
 
               // don't have an account? Register here
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Don't have an account? ", 
-                  style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary)
-                      ),
+                  Text(
+                    "Don't have an account? ",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                    ),
+                  ),
                   GestureDetector(
                     onTap: widget.onTap,
                     child: const Text(
@@ -137,7 +140,7 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                 ],
-              )
+              ),
             ],
           ),
         ),
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -6,9 +6,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
-  
   final void Function()? onTap;
-  
+
   const RegisterPage({super.key, required this.onTap});
 
   @override
@@ -26,7 +25,7 @@ class _RegisterPageState extends State<RegisterPage> {
   void registerUser() async {
     // show loading circle
     showDialog(
-      context: context, 
+      context: context,
       builder: (context) => const Center(
         child: CircularProgressIndicator(),
       ),
@@ -39,12 +38,9 @@ class _RegisterPageState extends State<RegisterPage> {
 
       // show error message to user
       displayMessageToUser("Passwords don't match!", context);
-    }
-
-    // if password match
-    else{
-      // try creating the user
-      try{
+    } else {
+      // if password match
+      try {
         // create the user
         UserCredential? userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: emailController.text,
@@ -70,9 +66,9 @@ class _RegisterPageState extends State<RegisterPage> {
   Future<void> createUserDocument(UserCredential? userCredential) async {
     if (userCredential != null && userCredential.user != null) {
       await FirebaseFirestore.instance
-      .collection("Users")
-      .doc(userCredential.user!.email)
-      .set({
+          .collection("Users")
+          .doc(userCredential.user!.email)
+          .set({
         "email": userCredential.user!.email,
         "username": usernameController.text,
         "bio": "Empty bio...", // default bio
@@ -85,77 +81,73 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.only(
+            left: 25.0,
+            right: 25.0,
+            top: 25.0,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 25.0,
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
               // logo
               Icon(
                 Icons.person,
                 size: 80,
                 color: Theme.of(context).colorScheme.inversePrimary,
               ),
-          
               const SizedBox(height: 25),
-          
+
               // app name
               const Text(
                 "Beauty Bazaar",
                 style: TextStyle(fontSize: 24),
               ),
-          
               const SizedBox(height: 50),
-          
+
               // username textfield
               MyTextField(
-                hintText: "Username", 
-                obscureText: false, 
+                hintText: "Username",
+                obscureText: false,
                 controller: usernameController,
               ),
-                        
               const SizedBox(height: 10),
 
               // email textfield
               MyTextField(
-                hintText: "Email", 
-                obscureText: false, 
+                hintText: "Email",
+                obscureText: false,
                 controller: emailController,
               ),
-                        
               const SizedBox(height: 10),
 
               // password textfield
               MyTextField(
-                hintText: "Password", 
-                obscureText: true, 
+                hintText: "Password",
+                obscureText: true,
                 controller: passwordController,
               ),
-
               const SizedBox(height: 10),
 
               // confirm password textfield
               MyTextField(
-                hintText: "Confirm Password", 
-                obscureText: true, 
+                hintText: "Confirm Password",
+                obscureText: true,
                 controller: confirmPwController,
               ),
-          
               const SizedBox(height: 10),
 
               // forgot password
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                    Text(
-                      "Forgot password?",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary)
-                    ),
+                  Text(
+                    "Forgot password?",
+                    style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+                  ),
                 ],
               ),
-          
               const SizedBox(height: 25),
 
               // register button
@@ -163,17 +155,16 @@ class _RegisterPageState extends State<RegisterPage> {
                 text: "Register",
                 onTap: registerUser,
               ),
-          
               const SizedBox(height: 25),
 
               // already have an account? Login here
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Already have an account? ", 
-                  style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary)
-                      ),
+                  Text(
+                    "Already have an account? ",
+                    style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+                  ),
                   GestureDetector(
                     onTap: widget.onTap,
                     child: const Text(
